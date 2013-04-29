911c2e6426a2093ddad3fefe0b5f954d8d860cfa
Shinichi Nishikawa <shinichi.nishikawa@gmail.com>
2013-04-30 05:06:29 +0900

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,34 @@ subtitle
 ========
 
 Display subtitle text field after the title in WordPress admin panel
+
+## How to use
+
+Once plugin activated, you get a subtitle input field after the title area in admin panel.
+
+### Retriving and displaying in templates
+
+Use `get_nskw_subtitle()` to retrieve and `nskw_subtitle()` to display.
+
+### Change label
+
+By default the label of the input field is "Subtitle".
+There's a hook for changing it.
+
+	add_filter( 'nskw-fat-meta_label', 'nskw_changeLabel' );
+	function nskw_changeLabel() {
+		return 'サブタイトル';
+	}
+
+### Hide in specific post types
+
+By default, subtitle field appears in every post type edit pages except for attachment.
+
+To hide in particular post type pages, there's a hook.
+
+	// hide subtitle field in posttype 'attachment', 'page'、''newposttype'
+	add_filter( 'nskw-fat_post_type', 'nskw_hide_subtitle' );
+	function nskw_hide_subtitle() {
+		return array( 'attachment', 'page', 'newposttype' );
+	}
+
---
+++ subtitle.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: Subtitle
+Plugin URI: http://nskw-style.com/2013/wordpress/plugin-downloads/subtitle.html ‎
+Description: This plugin adds simple meta field to wp-admin/post-new.php
+Author: Shinichi Nishikawa
+Version: 0.1
+Author URI: http://nskw-style.com
+*/
+
+class MetaFieldAfterTitle {
+
+public $meta_key = 'subtitle';
+public $meta_label = 'Subtitle';
+public $post_type = array( 'attachment' );
+
+// add actions
+function __construct()
+{
+	add_action( 'edit_form_after_title', array( &$this, 'edit_form_after_title' ) );
+	add_action( 'save_post', array( &$this, 'save_post' ) );
+	add_action( 'init', array( &$this, 'init' ) );
+}
+
+public function init() {
+	$this->meta_key = apply_filters( 'nskw-fat-meta_key', $this->meta_key );
+	$this->post_type = apply_filters( 'nskw-fat_post_type', $this->post_type );
+	$this->meta_label = apply_filters( 'nskw-fat-meta_label', $this->meta_label );
+	$this->post_id = '';
+	if ( isset($_GET['post']) ) {
+		$this->post_id = $_GET['post'];
+	} else {
+		global $post;
+		if ( !empty($post) ) {
+			$this->post_id = $post->ID;
+		}
+	}
+}
+
+// フォームの表示
+public function edit_form_after_title()
+{
+	// return if not admin panel
+	if ( !is_admin() ) {
+		return;
+	}
+	// return if post type differs
+	if ( in_array( get_post_type( $GLOBALS['post'] ), $this->post_type ) ) {
+		return;
+	}
+
+	$value = $this->get_the_subtitle();
+	printf(
+		'<p><label for="%1$s_id">%3$s <br /><input size="100" type="text" name="%1$s" id="%1$s_id" value="%2$s" /></label></p>',
+		$this->meta_key,
+		$value,
+		$this->meta_label
+	);		
+}
+
+// save the value
+public function save_post( $post_id )
+{
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
+    
+    if ( isset ( $_POST[ $this->meta_key ] ) ) {
+    	return update_post_meta( $post_id, $this->meta_key, $_POST[ $this->meta_key ] );
+    }
+    delete_post_meta( $post_id, $this->meta_key );
+
+}
+
+public function get_the_subtitle() {
+	global $post;
+	$the_subtitle = get_post_meta( $post->ID, $this->meta_key, true );
+	return esc_html( $the_subtitle );
+}
+
+}
+$nskwFAT = new MetaFieldAfterTitle();
+
+function get_nskw_subtitle() {
+	global $nskwFAT;
+	return $nskwFAT->get_the_subtitle();
+}
+
+function nskw_subtitle() {
+	echo get_nskw_subtitle();
+}
+
